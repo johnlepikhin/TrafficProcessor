470bb6fa4f2241a933b5c3730dd79bab8e64b8b3
Evgenii Lepikhin <johnlepikhin@gmail.com>
2016-08-12 17:24:19 +0300

    Init Data (Pcap API/parser)

---
+++ src/Data.cpp
@@ -0,0 +1,98 @@
+/*
+ * Pkt.cpp
+ *
+ *  Created on: 06 июня 2016 г.
+ *      Author: eugene
+ */
+
+#include "Data.h"
+
+#include <iostream>
+#include <sstream>
+#include <stdlib.h>
+#include <stdio.h>
+#include "core/utils.h"
+#include "types/MAC.h"
+
+Data::Data(std::istream *stream) :
+	Size(0),
+	Captured (0),
+	Position(0)
+{
+	stream->ignore(8);
+	util::anyRead (stream, &Captured, sizeof (Captured));
+	util::anyRead (stream, &Size, sizeof (Size));
+
+	size_t dataLength = Captured;
+	IS.resize(dataLength, '\0');
+	char *begin = &*IS.begin();
+	stream->read(begin, (long)dataLength);
+}
+
+Data::~Data() {
+}
+
+const char *Data::getPtrWithSize(std::streamsize size) {
+	const char *ptr = IS.c_str()+Position;
+	Position+=(unsigned long)size;
+	return (ptr);
+}
+
+const char *Data::getPtrAtOffset(std::streamoff offset) const {
+	const char *ptr = IS.c_str()+Position+offset;
+	return (ptr);
+}
+
+const void *Data::readAlloc(std::streamsize size) {
+	const char *r = IS.substr(Position, (unsigned long)size).c_str();
+	Position+=(unsigned long)size;
+
+	return ((const void *)r);
+}
+
+const void *Data::readAllocAhead(std::streamsize size, std::streamoff startpos = 0) const {
+	const char *r = IS.substr(Position+(unsigned long)startpos, (unsigned long)size).c_str();
+
+	return ((const void *)r);
+}
+
+void Data::read(void *buffer, std::streamsize size) {
+	IS.copy((char *)buffer, (unsigned long)size, Position);
+	Position+=(unsigned long)size;
+}
+
+void Data::readAhead(void *buffer, std::streamsize size, std::streamoff offset = 0) const {
+	IS.copy((char *)buffer, (unsigned long)size, Position+(unsigned long)offset);
+}
+
+unsigned short Data::read2() {
+	unsigned short int r = 0;
+	read(&r, 2);
+
+	return (r);
+}
+
+unsigned short Data::read2Reverse() {
+	unsigned short r = read2 ();
+
+	return (util::reverse2 (r));
+}
+
+void Data::ignore(std::streamsize size) {
+	Position+=(unsigned long)size;
+}
+
+bool Data::compare(std::string orig_string, std::streamsize offset) const {
+	long strLen = (long)orig_string.length();
+	std::string rdstr((const char *)readAllocAhead(strLen, offset));
+	return (0 == orig_string.compare(rdstr));
+}
+
+std::streamsize Data::bytesToEnd() const {
+	std::streamsize r = (std::streamsize)(IS.length()-Position);
+	if (r < 0) {
+		return (0);
+	} else {
+		return (r);
+	}
+}
---
+++ src/Data.h
@@ -0,0 +1,123 @@
+
+#ifndef DATA_H_
+#define DATA_H_
+
+#include <iostream>
+#include <sstream>
+
+/**
+ * Class that provides functionality to work with raw Pcap frames.
+ */
+class Data {
+public:
+	/**
+	 * Data constructor.
+	 * @param stream Input stream to get Pcap frame from
+	 */
+	Data (std::istream *stream);
+
+	~Data ();
+
+	/**
+	 * Get pointer to current position in buffer, update position according to passed value size.
+	 * @param size Length of the expected value
+	 * @return Pointer to first byte of value
+	 */
+	const char *getPtrWithSize (std::streamsize size);
+
+	/**
+	 * Get pointer to value at offset. Position in buffer will not be updated.
+	 * @param offset Offset relative to current position
+	 * @return Pointer to first byte of value
+	 */
+	const char *getPtrAtOffset (std::streamoff offset) const;
+
+	/**
+	 * Read value of specified size, save it to newly allocated memory block and return pointer
+	 * to it. Update position in buffer to new offset.
+	 * @param size Size of the expected value
+	 * @return Pointer to first byte of new value
+	 */
+	const void *readAlloc (std::streamsize size);
+
+	/**
+	 * Read value of specified size at specified offset (relative to current position), save
+	 * it to newly allocated memory block and return pointer to it. Position will not be updated.
+	 * @param size Size of the expected value
+	 * @param offset Offset relative to current position
+	 * @return Pointer to first byte of new value
+	 */
+	const void *readAllocAhead (std::streamsize size, std::streamoff offset) const;
+
+	/**
+	 * Read value of specified size, save it to already allocated memory block. Update
+	 * position in buffer to new offset.
+	 * @param buffer Pointer to output buffer
+	 * @param size Size of the expected value
+	 */
+	void read (void *buffer, std::streamsize size);
+
+	/**
+	 * Read value of specified size at specified offset, save it to already allocated
+	 * memory block. Position will not be updated.
+	 * @param buffer Pointer to output buffer
+	 * @param size Size of the expected value
+	 * @param offset Offset relative to current position
+	 */
+	void readAhead (void *buffer, std::streamsize size, std::streamoff offset) const;
+
+	/**
+	 * Read two bytes and return it as int. Update position in buffer to new offset.
+	 * @return Result value
+	 */
+	unsigned short read2 ();
+
+	/**
+	 * Same as Data::read2, but returns bytes in reverse order.
+	 * @see Data::read2
+	 * @return Result value
+	 */
+	unsigned short read2Reverse ();
+
+	/**
+	 * Skip next N characters (update buffer offset)
+	 * @param characters number of characters to skip
+	 */
+	void ignore (std::streamoff characters);
+
+	/**
+	 * Compare string with buffer at specified position.
+	 * @param orig_string String to compare with
+	 * @param offset Offset in buffer (relative to current position) to compare with
+	 * @return
+	 */
+	bool compare (std::string orig_string, std::streamoff offset) const;
+
+	/**
+	 * Returns number of bytes in buffer from current position to the end
+	 * @return Number of bytes
+	 */
+	std::streamsize bytesToEnd () const;
+
+	/**
+	 * Total size of captured packet.
+	 */
+	unsigned short Size;
+
+	/**
+	 * Number of captured bytes.
+	 */
+	unsigned short Captured;
+
+	/**
+	 * Input buffer
+	 */
+	std::string IS;
+
+	/**
+	 * Current position in buffer Data::IS
+	 */
+	unsigned long Position;
+};
+
+#endif /* DATA_H_ */

739eae048416e6f11caf1783743700f6d0e6d1ff
Evgenii Lepikhin <johnlepikhin@gmail.com>
2016-08-12 17:22:32 +0300

    Add utils

---
+++ src/core/utils.cpp
@@ -0,0 +1,54 @@
+/*
+ * utils.cpp
+ *
+ *  Created on: 06 июня 2016 г.
+ *      Author: eugene
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+
+
+namespace util {
+
+	void exitFatal (const char *msg) {
+		fprintf (stderr, "Fatal error: %s\n", msg);
+		exit (1);
+	}
+
+	void exitMemoryError () {
+		exitFatal ("Cannot allocate memory");
+	}
+
+	void *mallocOrFatal (size_t size) {
+		void *r = malloc(size);
+		if (NULL == r) {
+			exitMemoryError();
+		}
+
+		return (r);
+	}
+
+	void *mallocRead (std::istream *stream, std::streamsize size) {
+		void *r = (void *)mallocOrFatal((size_t)size);
+		stream->read((char *)r, size);
+		if (stream->fail ()) {
+			free (r);
+			return (NULL);
+		}
+
+		return (r);
+	}
+
+	void anyRead (std::istream *stream, const void *ptr, std::streamsize size) {
+		stream->read((char *)ptr, size);
+		if (stream->fail ()) {
+			throw -1;
+		}
+	}
+
+	unsigned short reverse2 (unsigned short v) {
+		return (((v & 0xff) << 8) + (v >> 8));
+	}
+}
---
+++ src/core/utils.h
@@ -0,0 +1,57 @@
+/*
+ * utils.h
+ *
+ *  Created on: 06 июня 2016 г.
+ *      Author: eugene
+ */
+
+#ifndef UTILS_H_
+#define UTILS_H_
+
+#include <istream>
+
+namespace util {
+
+	/**
+	 * Exit immediately with error message
+	 * @param msg Error message to show
+	 */
+	void exitFatal (const char *msg);
+
+	/**
+	 * Exit with memory error message
+	 */
+	void exitMemoryError ();
+
+	/**
+	 * <malloc>"()" memory of specified size, check if allocation was successful and call
+	 * <exit_memory>"()" if pointer is NULL. Return pointer to the block otherwise.
+	 * @param size Size of block to be allocated
+	 * @return Pointer to successfully allocated block
+	 */
+	void *mallocOrFatal (size_t size);
+
+	/**
+	 * <mallocOrFatal>"(size)" memory and read bytes from stream into it
+	 * @param stream Input stream
+	 * @param size Number of bytes to read
+	 */
+	void *mallocRead (std::istream *stream, std::streamsize size);
+
+	/**
+	 * Read arbitrary number of bytes from input stream into specified memory block
+	 * @param stream Input stream
+	 * @param ptr Pointer to allocated memory block
+	 * @param size Number of bytes to read
+	 */
+	void anyRead (std::istream *stream, const void *ptr, std::streamsize size);
+
+	/**
+	 * Return 2-byte input value with reversed byte order
+	 * @param value Value to reverse
+	 * @return Reversed value
+	 */
+	unsigned short reverse2 (unsigned short value);
+}
+
+#endif /* UTILS_H_ */

4fa8fef97904cfc7651e5b95ff20338adf822ab3
Evgenii Lepikhin <johnlepikhin@gmail.com>
2016-08-16 15:36:13 +0300

    Add Parser template, minor changes

--- src/types/Chunk.cpp
+++ src/types/Chunk.cpp
@@ -1,16 +1,28 @@
 
 #include "Chunk.h"
 
-Chunk::Chunk(Data *data, Chunk *parent)
+Chunk::Chunk(const Data * const data, Chunk *parent)
 	: DataPtr(data)
 	, DataPosition(data->Position)
 	, Parent(parent)
 {
 }
 
-Chunk::Chunk(Data *data)
-: DataPtr(data)
-, DataPosition(data->Position)
-, Parent(NULL)
+Chunk::Chunk(const Data *data)
+	: DataPtr(data)
+	, DataPosition(data->Position)
+	, Parent(NULL)
 {
+}
+
+template <class PARENT, class RETURN>
+Parser<PARENT, RETURN>::Parser ()
+	: Followers(std::vector<Parser<PARENT, RETURN> >())
+{
+}
+
+template <class PARENT, class RETURN>
+std::vector<Parser<PARENT, RETURN> > *Parser<PARENT, RETURN>::GetFollowers ()
+{
+	return (Followers);
 }
--- src/types/Chunk.h
+++ src/types/Chunk.h
@@ -2,6 +2,8 @@
 #ifndef SRC_CORE_CHUNK_H_
 #define SRC_CORE_CHUNK_H_
 
+#include <string>
+#include <vector>
 #include "Data.h"
 
 /**
@@ -14,18 +16,20 @@ public:
 	 * @param data Reference to data (original Pcap)
 	 * @param parent Reference to parent Chunk
 	 */
-	Chunk(Data *data, Chunk *parent);
+	Chunk(const Data * const data, Chunk *parent);
 
 	/**
 	 * Construct Chunk from Data and NULL parent
 	 * @param data Reference to data (original Pcap)
 	 */
-	Chunk(Data *data);
+	Chunk(const Data *data);
+
+//	virtual ~Chunk();
 
 	/**
 	 * Reference to original Pcap
 	 */
-	Data *DataPtr;
+	const Data *DataPtr;
 
 	/**
 	 * Pointer to position in Data where Chunk begins
@@ -35,7 +39,21 @@ public:
 	/**
 	 * Pointer to parent Chunk
 	 */
-	Chunk *Parent;
+	const Chunk *Parent;
+};
+
+template <class PARENT, class RETURN>
+class Parser {
+private:
+	std::vector<Parser> Followers;
+public:
+	Parser();
+	virtual ~Parser();
+	virtual RETURN *parse(PARENT *data);
+	virtual std::string ID();
+	virtual std::string Description();
+	std::vector<Parser> *GetFollowers();
+	void SetFollowers(std::vector<Parser> *followers);
 };
 
 #endif /* SRC_CORE_CHUNK_H_ */

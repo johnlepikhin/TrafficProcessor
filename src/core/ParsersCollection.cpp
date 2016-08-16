f14dc3b10f6cfb9c9ec15e3497d2de3199de3564
Evgenii Lepikhin <johnlepikhin@gmail.com>
2016-08-16 15:27:51 +0300

    Initialize ParsersCollection

---
+++ src/core/ParsersCollection.cpp
@@ -0,0 +1,10 @@
+/*
+ * ParsersCollection.cpp
+ *
+ *  Created on: 16 авг. 2016 г.
+ *      Author: eugene
+ */
+
+#include "ParsersCollection.h"
+
+ParsersCollection *ParsersCollection::p_instance = 0;
---
+++ src/core/ParsersCollection.h
@@ -0,0 +1,30 @@
+/*
+ * ParsersCollection.h
+ *
+ *  Created on: 16 авг. 2016 г.
+ *      Author: eugene
+ */
+
+#ifndef SRC_CORE_PARSERSCOLLECTION_H_
+#define SRC_CORE_PARSERSCOLLECTION_H_
+
+#include <vector>
+#include "../types/Chunk.h"
+
+class ParsersCollection {
+private:
+    static ParsersCollection * p_instance;
+    std::vector<Parser<void, void> > collection;
+	ParsersCollection& operator= (const ParsersCollection &);
+public:
+	ParsersCollection() {};
+	ParsersCollection( const ParsersCollection& ) {};
+	static ParsersCollection *getInstance() {
+		if (!p_instance) {
+			p_instance = new ParsersCollection();
+		}
+		return (p_instance);
+	}
+};
+
+#endif /* SRC_CORE_PARSERSCOLLECTION_H_ */

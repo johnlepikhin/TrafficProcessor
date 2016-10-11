2fc85fab8e7e472abbb17ec68cf2af2ee17519db
Evgenii Lepikhin <johnlepikhin@gmail.com>
2016-10-11 16:34:14 +0300

    Improvements for TCP chunk printer

--- src/levels/PrinterTCP.cpp
+++ src/levels/PrinterTCP.cpp
@@ -16,15 +16,27 @@ std::shared_ptr<ChunkRaw> PrinterTCP::Process(std::shared_ptr<ChunkTCP> chunk)
 {
 	try {
 		std::string *payload = chunk->Payload->GetSubStringOrFail(0, 20);
-		std::cout << "TCP " << chunk->SourcePort
-				<< " " << chunk->DestinationPort
+		std::cout << "TCP srcp=" << chunk->SourcePort
+				<< " dstp=" << chunk->DestinationPort
+				<< " seq=" << chunk->SeqNumber
+				<< " ack=" << chunk->ConfirmNumber
+				<< " flgACK=" << chunk->FlagACK
+				<< " flgSYN=" << chunk->FlagSYN
+				<< " flgFIN=" << chunk->FlagFIN
 				<< " length=" << chunk->PayloadLength
 				<< " content=" << payload[0]
 				<< "\n";
 
 		delete payload;
 	} catch (...) {
-
+		std::cout << "TCP srcp=" << chunk->SourcePort
+				<< " dstp=" << chunk->DestinationPort
+				<< " seq=" << chunk->SeqNumber
+				<< " ack=" << chunk->ConfirmNumber
+				<< " flgACK=" << chunk->FlagACK
+				<< " flgSYN=" << chunk->FlagSYN
+				<< " flgFIN=" << chunk->FlagFIN
+				<< "\n";
 	}
 
 	return (std::shared_ptr<ChunkRaw>(nullptr));
